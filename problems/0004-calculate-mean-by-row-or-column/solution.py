6c8b60744a825142096a814136513fc750dea557
DebanganMandal <58435489+DebanganMandal@users.noreply.github.com>
2026-09-18 08:03:01 +0000

    Update: Calculate Mean by Row or Column

--- problems/0004-calculate-mean-by-row-or-column/solution.py
+++ problems/0004-calculate-mean-by-row-or-column/solution.py
@@ -1,26 +1,33 @@
 import numpy as np
 
-def row_wise_sum(matrix:np.ndarray):
-	means = []
-	for row in matrix:
-		s = 0
-		for n in row:
-			s += n
-		means.append(s)
-	return means
+# def row_wise_sum(matrix:np.ndarray):
+# 	means = []
+# 	for row in matrix:
+# 		s = 0
+# 		for n in row:
+# 			s += n
+# 		means.append(s)
+# 	return means
+
+# def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
+# 	means = []
+# 	m = len(matrix)
+# 	n = len(matrix[0])
+
+# 	matrix = np.array(matrix)
+
+# 	if mode == "row": 
+# 		means = row_wise_sum(matrix)
+# 		means = [float(x/n) for x in means]
+# 	else: 
+# 		means = row_wise_sum(matrix.T)
+# 		means = [float(x/m) for x in means]
+
+# 	return means
+
 
 def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
-	means = []
-	m = len(matrix)
-	n = len(matrix[0])
-
 	matrix = np.array(matrix)
-
-	if mode == "row": 
-		means = row_wise_sum(matrix)
-		means = [float(x/n) for x in means]
-	else: 
-		means = row_wise_sum(matrix.T)
-		means = [float(x/m) for x in means]
-
-	return means
+	
+	if mode == "row": return matrix.mean(axis=1).tolist()
+	else: return matrix.mean(axis=0).tolist()

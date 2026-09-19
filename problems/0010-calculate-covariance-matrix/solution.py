1b6836acc7afcccae27c15235ea6d961e6ee126a
DebanganMandal <58435489+DebanganMandal@users.noreply.github.com>
2026-09-19 06:36:39 +0000

    Update: Calculate Covariance Matrix

--- problems/0010-calculate-covariance-matrix/solution.py
+++ problems/0010-calculate-covariance-matrix/solution.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
 	X = np.array(vectors, dtype=np.float32)
-	return np.cov(X)
+	return np.cov(X).tolist()
 
 # def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
 # 	X = np.array(vectors, dtype=np.float64)

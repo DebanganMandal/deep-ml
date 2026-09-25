d5d1fb92e2051cbc4b998949c1afb578d1fcb1af
DebanganMandal <58435489+DebanganMandal@users.noreply.github.com>
2026-09-25 15:44:57 +0000

    Update: 2D Translation Matrix Implementation

--- problems/0055-2d-translation-matrix-implementation/solution.py
+++ problems/0055-2d-translation-matrix-implementation/solution.py
@@ -1,8 +1,26 @@
 import numpy as np
 def translate_object(points, tx, ty):
-	points = np.array(points, dtype=float)
-	T = np.array([tx, ty])
-	translated_points = []
+	# points = np.array(points, dtype=float)
+	# T = np.array([tx, ty])
+	# translated_points = []
+	# for p in points:
+	# 	translated_points.append((p+T).tolist())
+	# return translated_points
+
 	for p in points:
-		translated_points.append((p+T).tolist())
-	return translated_points
+		p.append(1)
+
+	points = np.array(points, dtype=float)
+	T = np.eye(3)
+
+	T[0, -1] = tx
+	T[1, -1] = ty
+
+	# print(points[0].shape)
+	P = T @ points.T
+	
+	P = P[:-1,:].T
+	
+	return P.tolist()
+
+
